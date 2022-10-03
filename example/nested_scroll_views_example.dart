217963a2961c8ef8c885973c9ade152546e3447b
Flop <kukuzuo@gmail.com>
2022-10-03 14:11:03 +0800

    init

---
+++ example/nested_scroll_views_example.dart
@@ -0,0 +1,6 @@
+import 'package:nested_scroll_views/nested_scroll_views.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/nested_scroll_views_test.dart
@@ -0,0 +1,16 @@
+import 'package:nested_scroll_views/nested_scroll_views.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
